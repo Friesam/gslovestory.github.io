c90da1a9d4ad93565619557f70ee3660bf24cc1e
fnm757@mun.ca <frieda@Friedas-MacBook-Air.local>
2021-03-11 22:15:53 -0330

    made more changes

--- rsvp.js
+++ rsvp.js
@@ -1,38 +1,27 @@
+// import { get } from "http";
+
 $(document).ready(function() {
-    // $("#detailsForm").hide();
-
-    var getFirstName = $('input[name=firstname]').val();
-    var getLastName = $('input[name=lastname]').val();
-
-    // this.showhide();
+  
     $("#nameButton").click(function() {
+      let inputFirstName = $("#firstName").val();
+      let inputLastName = $("lastName").val();
+
       $("#detailsEntry").show();
       $("#detailsForm").hide();
       $("#nameButtom").hide();
-    });
-    // this.showhide();
-    // function showhide(){
-    //   $("#nameForm").ready(function() {
-    //     $("#acceptInvitation").hide();
-    //   $("#decideInvitation").hide();
-    //   $("#nameButtom").hide();
-    //   })
-    // };
+  
 
-    // $("#accept").click(function () {
-    //     $("#acceptNote").show();
-    //     $("#acceptnote").show();
-    //     $("#decideInvitation").show();
-    //     $("#declineInvitation").toggle();
-    // });
-    
-    // $("#decline").click(function () {
-    //     $("#acceptInvitation").toggle();
-    //     $("#couplenote").show();
-    //     $("#wisdomeParagraph").show();
-    //     $("#wisdomnote").show();
-    //     $("#decideInvitation").show();
-    // });
+      $('#firstName').val()
+      $("#lastName").val()
+     
+      $('.name-text').text($('#firstName').val() + " " + $("#lastName").val());
+     
+    });
+   
+    $('#submitRsvp').click(function() {
+      $('.completed').text('RSVP Completely Filled!!!')
+      // alert("RSVP completed")
+    })
     let rsvpInfo = {
       "phoneNumber": "8675309",
       "firstName": "value",
