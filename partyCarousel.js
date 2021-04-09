89ee2e53db1fb3f9618f7f8524100d2416539f4e
fnm757@mun.ca <frieda@Friedas-MacBook-Air.local>
2021-04-08 22:17:34 -0230

    wedding gallery

---
+++ partyCarousel.js
@@ -0,0 +1,12 @@
+(function () {
+    function getSlideParameter(key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+        var match = location.search.match(new RegExp("[?&]" + key + "=([^&]+)(&|$)"));
+        var slide = match && decodeURIComponent(match[1].replace(/\+/g, " "));
+        if (Math.floor(slide) == slide && $.isNumeric(slide))
+            return parseInt(slide);
+        else
+            return 0;//if 'slide' parameter is not present or doesn't have correct values load 0th slide
+    }
+    $('#carouselExampleIndicators').carousel(getSlideParameter('slide'));
+})();
