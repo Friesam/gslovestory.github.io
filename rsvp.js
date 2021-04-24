7d5d99d51b14d961fd0c03a5110677f99d62d723
fnm757@mun.ca <frieda@Friedas-MacBook-Air.local>
2021-04-24 20:15:03 -0230

    removed js

--- rsvp.js
+++ rsvp.js
@@ -22,24 +22,6 @@ $(document).ready(function() {
       $('.completed').text('RSVP Completely Filled!!!')
       // alert("RSVP completed")
     })
-    let rsvpInfo = {
-      "phoneNumber": "8675309",
-      "firstName": "value",
-      "lastName": "value",
-      "accept": "value",
-      "decline": "value",
-      "coupleNote": "value",
-      "wisdomNote": "value",
-      "lastName": "value"
-  }
-  $.ajax({
-    type: 'put',
-    url: 'https://l6a4uu9pxh.execute-api.us-east-2.amazonaws.com/v1/store-info',
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(rsvpInfo),
-    })
-    .done(function (results) {
-      console.log(results);
-    })
+    
   });
   
